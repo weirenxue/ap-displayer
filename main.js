d560e6114612c69cd1e460dfdb9d12537d54cd97
wrxue <a0981355215@gmail.com>
2021-01-16 23:57:12 +0800

    視窗縮小時隱藏、出現在系統匣

--- main.js
+++ main.js
@@ -1,30 +1,68 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu, Tray } = require('electron');
+const path = require('path');
 
+let win = null;
 function createWindow () {
-    const win = new BrowserWindow({
+    win = new BrowserWindow({
         width: 1200,
         height: 1200,
+        icon: path.join(__dirname, 'wrx512.ico'),
         webPreferences: {
             nodeIntegration: true,
             enableRemoteModule: true,
         }
-    })
+    });
 
-    win.loadFile('index.html')
+    win.loadFile('index.html');
+
+    win.on('minimize', () => {
+        win.hide()
+    });
+
+    createTray();
+}
+let appIcon = null;
+function createTray() {
+    appIcon = new Tray(path.join(__dirname, 'wrx512.ico'));
+    const contextMenu = Menu.buildFromTemplate([
+        {
+            label: '顯示',
+            click() {
+                win.show();
+            }
+        },
+        {
+            label: '離開',
+            click() {
+                win.removeAllListeners('close');
+                win.close();
+            }
+        }
+    ]);
+    appIcon.on('click', function(e){
+        if (win.isVisible()) {
+            win.hide()
+        } else {
+            win.show()
+        }
+    });
+    appIcon.setIgnoreDoubleClickEvents(true);
+    appIcon.setToolTip('A/P Displayer');
+    appIcon.setContextMenu(contextMenu);
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
     console.log('window-all-closed')
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
-})
+});
 
 app.on('activate', () => {
-    console.log('activate')
+    console.log('activate');
     if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow()
+        createWindow();
     }
-})
+});
